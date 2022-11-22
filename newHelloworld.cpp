c2c3d19dcaffad42309944161278d923fc67baf4
bochevis <boch@viken.no>
2022-11-22 10:20:22 +0100

    oppgave2

---
+++ newHelloworld.cpp
@@ -0,0 +1,24 @@
+// newHelloworld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include <string>
+using namespace std;
+int main()
+{
+    string skrivher;
+    std::cout << "skriv hva som helst: ";
+
+    getline(cin, skrivher);
+    cout << "Du skrev: " << skrivher << "!" << endl;
+
+
+
+  
+    
+
+
+    system("pause>0");
+}
+
